75d86c63efc10c74a5bd9a64864420fda7b56dc7
Helen Marshall <marshallhelenm@gmail.com>
2019-07-02 15:06:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+def map(source_array, block)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    n = source_array[i]
+    new_array << yield
+    i +=1 
+  end
+  return new_array  
+end 
+
+def reduce(source_array, starting_point = 0)
+  value = starting_point
+  i = 0 
+  while i < source_array.length do
+    yield
+    i += 1
+  end
+ return value
+end
